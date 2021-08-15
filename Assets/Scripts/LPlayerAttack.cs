504c0a23d0720efd20ebcf8dedb383084099b7a5
archria <jonghyun95@kakao.com>
2021-08-15 23:09:01 +0900

    player attack add

--- Assets/Scripts/LPlayerAttack.cs
+++ Assets/Scripts/LPlayerAttack.cs
@@ -7,17 +7,22 @@ public class LPlayerAttack : MonoBehaviour
 
     public GameObject bolt;
     public Transform pos;
+    SpriteRenderer spriteRenderer;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftControl)){
-            Instantiate(bolt, pos.position, transform.rotation);
+        if(Input.GetKeyDown(KeyCode.LeftControl) && spriteRenderer.flipX == true){
+            Instantiate(bolt, pos.position, Quaternion.Euler(0,180,0) );
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftControl) && spriteRenderer.flipX == false){
+            Instantiate(bolt, pos.position, Quaternion.Euler(0,0,0) );
         }
     }
 }
--- Assets/Scripts/Lbullet.cs
+++ Assets/Scripts/Lbullet.cs
@@ -9,16 +9,36 @@ public class Lbullet : MonoBehaviour
     public float speed;
     public GameObject spark;
     Vector3 pos;
+    public float h;
+    public float distance;
+    public LayerMask isLayer;
+    
+    SpriteRenderer spriteRenderer;
     void Start()
-    {
+    {   
+        h = Input.GetAxisRaw("Horizontal");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //spriteRenderer.flipX = h == 1;
         Invoke("DestroyBullet",1.5f);
     }
 
     // Update is called once per frame
     void Update(){
-
-        transform.Translate(transform.right * speed * Time.deltaTime);
         
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, transform.right, distance, isLayer);
+        if(ray.collider != null){
+            if(ray.collider.tag == "Enemy" || ray.collider.tag == "Platform"){
+                DestroyBullet();
+            }
+        }
+              
+         if(transform.rotation.y == 0){
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+        else{
+            transform.Translate(transform.right * speed * -1 * Time.deltaTime);
+        }
+
     }
     void DestroyBullet(){
         Destroy(gameObject);
